76d6da2c4b9f098051c2ab98858c3b4340391967
Sorano7 <sorano27@outlook.com>
2026-09-20 16:20:24 +0800

    refactor: adjust project structure for library

--- cut.c
+++ cut.c
@@ -1,44 +1,54 @@
 #define CUT_IMPL
-#include "include/cut.h"
+#include "src/include/cut.h"
 
-void shared_config(CutUnit *u)
+void core_config(CutUnit *u)
 {
-    cut_unit_sources(u, "src/ast.c");
-    cut_unit_sources(u, "src/creal.c");
-    cut_unit_sources(u, "src/number.c");
-    cut_unit_sources(u, "src/lexer.c");
-    cut_unit_sources(u, "src/parser.c");
-    cut_unit_sources(u, "src/value.c");
-    cut_unit_sources(u, "src/vm.c");
+    cut_unit_sources(u, "src/core/ast.c");
+    cut_unit_sources(u, "src/core/creal.c");
+    cut_unit_sources(u, "src/core/number.c");
+    cut_unit_sources(u, "src/core/lexer.c");
+    cut_unit_sources(u, "src/core/parser.c");
+    cut_unit_sources(u, "src/core/value.c");
+    cut_unit_sources(u, "src/core/vm.c");
 
-    cut_unit_includes(u, "include");
+    cut_unit_includes(u, "src/include");
     cut_unit_flags(u, "-g", "-Wall", "-Wextra", "-Wno-override-init");
     cut_unit_flags(u, "-static");
     // cut_unit_flags(u, "-fsanitize=address,undefined");
     cut_unit_libs(u, "mpfi", "mpfr", "gmp", "m");
 }
 
+void test_config(CutUnit *u)
+{
+    cut_unit_init(u, "test", CUT_UNIT_EXE);
+    cut_unit_sources(u, "tests/main.c");
+    cut_unit_sources(u, "tests/creal.c");
+    cut_unit_sources(u, "tests/lexer.c");
+    cut_unit_sources(u, "tests/parser.c");
+    cut_unit_sources(u, "tests/vm.c");
+    core_config(u);
+}
+
+void cli_config(CutUnit *u)
+{
+
+    cut_unit_init(u, "cli", CUT_UNIT_EXE);
+    cut_unit_sources(u, "src/cli/main.c");
+    cut_unit_sources(u, "src/cli/repl.c");
+    cut_unit_libs(u, "readline", "ncursesw");
+    core_config(u);
+}
+
 int main(int argc, char **argv)
 {
     cut_build_init();
 
-    CutUnit app;
-    cut_unit_init(&app, "ccal", CUT_UNIT_EXE);
-    cut_unit_sources(&app, "src/main.c");
-    cut_unit_libs(&app, "readline", "ncursesw", "tinfow");
-    cut_unit_sources(&app, "src/repl.c");
-    shared_config(&app);
+    CutUnit cli;
+    cli_config(&cli);
 
     CutUnit test;
-    cut_unit_init(&test, "test", CUT_UNIT_EXE);
-    cut_unit_sources(&test, "tests/main.c");
-    cut_unit_sources(&test, "tests/creal.c");
-    cut_unit_sources(&test, "tests/lexer.c");
-    cut_unit_sources(&test, "tests/parser.c");
-    cut_unit_sources(&test, "tests/vm.c");
-    shared_config(&test);
+    test_config(&test);
 
-    cut_build_add(&app, &test);
-
+    cut_build_add(&cli, &test);
     return cut_build_run(argc, argv);
 }
